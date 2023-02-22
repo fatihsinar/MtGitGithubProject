ea471d6839e21a34c56f147731c45d17ab74660a
Fatih Sınar <fatihsinar@gmail.com>
2023-02-22 21:31:31 +0300

    v4

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -4,5 +4,7 @@ public class AtmApp {
         //version 2
         System.out.println("Java is easy");
         System.out.println("Learn Java Earn Money");
+        //version 4
+        System.out.println("Giti öğrenmek çok kolay");
     }
 }
